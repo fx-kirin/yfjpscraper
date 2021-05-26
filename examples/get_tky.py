c7cc4b973acd51eab44c9e92430d43424860f632
fx_kirin <fx.kirin@gmail.com>
2021-05-26 09:02:44 +0900

    fix for etf

--- examples/get_tky.py
+++ examples/get_tky.py
@@ -1,9 +1,9 @@
 import datetime
 from yfjpscraper import get_data
 
-tick_id = 2678
+tick_id = 1357
 start_dt = datetime.date(2017, 1, 1)
-end_dt = datetime.date(2021, 6, 1)
+end_dt = datetime.date(2017, 6, 1)
 resp = get_data(tick_id, start_dt, end_dt)
 
 for data in resp:
