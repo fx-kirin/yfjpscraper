e4f39aca727b70abc0c2589c1dc961d3e86dcf01
fx_kirin <fx.kirin@gmail.com>
2022-11-29 13:34:38 +0900

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ deps = [
 
 setup(
     name="yfjpscraper",
-    version="0.2.2",
+    version="0.2.3",
     description="yahoo finance japan parser",
     author="fx-kirin",
     author_email="fx.kirin@gmail.com",
